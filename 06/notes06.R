b5338009c649543196a8899dc3bfff2cea433511
Edward Ionides <ionides@umich.edu>
2020-02-03 19:08:53 -0500

    notes06 posted

--- 06/notes06.R
+++ 06/notes06.R
@@ -29,10 +29,22 @@ dat$year <- as.numeric(format(dat$Date, format="%Y"))
 dat$month <- as.numeric(format(dat$Date, format="%m"))
 
 
-## ----plot_data-----------------------------------------------------------
-head(dat)
+## ----head_huron_data,echo=T,eval=T---------------------------------------
+head(dat,3)
+
+
+## ----plot_data_code,echo=T,eval=F----------------------------------------
+## huron_depth <- dat$Average
+## time <- dat$year + dat$month/12
+## # Note: we treat December 2011 as time 2012.0, etc
+## plot(huron_depth~time,type="l")
+
+
+## ----plot_data,echo=F,eval=T,out.width="8cm",fig.width=5,fig.height=2.5----
+par(mai=c(0.8,0.8,0.1,0.1))
 huron_depth <- dat$Average
-time <- dat$year + dat$month/12 # Note: we treat December 2011 as time 2012.0, etc
+time <- dat$year + dat$month/12 
+# Note: we treat December 2011 as time 2012.0, etc
 plot(huron_depth~time,type="l")
 
 
@@ -41,26 +53,37 @@ huron_sarma11x10 <- arima(huron_depth,
    order=c(1,0,1),
    seasonal=list(order=c(1,0,0),period=12)
 )
+
+
+## ----sarima_summary------------------------------------------------------
 huron_sarma11x10
 
 
-## ----residuals-----------------------------------------------------------
+## ----residuals,echo=F,out.width="8cm",fig.width=5,fig.height=2.5---------
+par(mai=c(0.8,0.8,0.1,0.1))
 acf(resid(huron_sarma11x10))
 
 
-## ----data_subset---------------------------------------------------------
+## ----data_subset,echo=F,out.width="8cm",fig.width=5,fig.height=2.5-------
 monthly_dat <- subset(dat, month==1)
 huron <- monthly_dat$Average
 year <- monthly_dat$year
+par(mai=c(0.8,0.8,0.1,0.1))
 plot(x=year,y=huron,type="l")
 
 
 ## ----h0_fit--------------------------------------------------------------
 fit0 <- arima(huron,order=c(1,0,0))
+
+
+## ----h0_summary,echo=F---------------------------------------------------
 fit0
 
 
 ## ----h1_fit--------------------------------------------------------------
 fit1 <- arima(huron,order=c(1,0,0),xreg=year)
+
+
+## ----h1_summary,echo=F---------------------------------------------------
 fit1
 
