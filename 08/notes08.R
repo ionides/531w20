a3e5910846650dc3d11a82809121f425f0113eae
Edward Ionides <ionides@umich.edu>
2020-02-10 17:44:46 -0500

    notes8 posted

--- 08/notes08.R
+++ 08/notes08.R
@@ -1,4 +1,4 @@
-## ----setup,echo=F,results=F,cache=F-------------------------------------------
+## ----setup,echo=F,results=F,cache=F--------------------------------------
 myround<- function (x, digits = 1) {
   # taken from the broman package
   if (digits < 1) 
@@ -22,20 +22,28 @@ options(
 
 
 
-## ----data_unadj---------------------------------------------------------------
+## ----data_unadj----------------------------------------------------------
 system("head unadjusted_unemployment.csv",intern=TRUE)
-U1 <- read.table(file="unadjusted_unemployment.csv",sep=",",header=TRUE)
-head(U1)
+U1 <- read.table(file="unadjusted_unemployment.csv",
+  sep=",",header=TRUE)
+head(U1,3)
 
 
-## ----reshape------------------------------------------------------------------
+## ----reshape_code,echo=T,eval=F------------------------------------------
+## u1 <- t(as.matrix(U1[2:13]))
+## dim(u1) <- NULL
+## date <- seq(from=1948,length=length(u1),by=1/12)
+## plot(date,u1,type="l",ylab="Percent unemployment (unadjusted)")
+
+
+## ----reshape,echo=F,out.width="10cm"-------------------------------------
 u1 <- t(as.matrix(U1[2:13]))
 dim(u1) <- NULL
 date <- seq(from=1948,length=length(u1),by=1/12)
 plot(date,u1,type="l",ylab="Percent unemployment (unadjusted)")
 
 
-## ----data_adj-----------------------------------------------------------------
+## ----data_adj_code,echo=F,eval=T,out.width="10cm"------------------------
 U2 <- read.table(file="adjusted_unemployment.csv",sep=",",header=TRUE)
 u2 <- t(as.matrix(U2[2:13]))
 dim(u2) <- NULL
@@ -44,38 +52,107 @@ lines(date,u2,type="l",col="red")
 title("Unemployment. Raw (black) and seasonally adjusted (red)")
 
 
-## ----plot.ts------------------------------------------------------------------
+## ----adjustment_spectrum_code,eval=F,echo=T------------------------------
+## u1_ts <- ts(u1,start=1948,frequency=12)
+## u2_ts <- ts(u2,start=1948,frequency=12)
+## spectrum(ts.union(u1_ts,u2_ts),spans=c(3,5,3),
+##   main="Unemployment. Raw (black) and seasonally adjusted (red)")
+
+
+## ----adjustment_spectrum,eval=T,echo=F,out.width="10cm"------------------
+u1_ts <- ts(u1,start=1948,frequency=12)
+u2_ts <- ts(u2,start=1948,frequency=12)
+spectrum(ts.union(u1_ts,u2_ts),spans=c(3,5,3),
+  main="Unemployment. Raw (black) and seasonally adjusted (red)")
+
+
+## ----plot.ts_code,echo=T,eval=F------------------------------------------
+## plot(u1_ts)
+
+
+## ----plot.ts,echo=F,out.width="10cm"-------------------------------------
+par(mai=c(0.8,0.8,0.1,0.1))
 plot(u1_ts)
 
 
-## ----bls_filter---------------------------------------------------------------
+## ----bls_filter----------------------------------------------------------
 s <- spectrum(ts.union(u1_ts,u2_ts),plot=FALSE)
 
 
-## ----s_names------------------------------------------------------------------
+## ----s_names-------------------------------------------------------------
 names(s)
 
 
-## ----s_spec_dim---------------------------------------------------------------
-dim(s$spec)
+## ----s_transfer_code,eval=F,echo=T---------------------------------------
+## plot(s$freq,s$spec[,2]/s$spec[,1],type="l",log="y",
+##   ylab="frequency ratio", xlab="frequency",
+##   main="frequency response (dashed lines at 0.9 and 1.1)")
+## abline(h=c(0.9,1.1),lty="dashed",col="red")
 
 
-## ----s_transfer---------------------------------------------------------------
+## ----s_transfer,eval=T,echo=F,out.width="10cm"---------------------------
 plot(s$freq,s$spec[,2]/s$spec[,1],type="l",log="y",
   ylab="frequency ratio", xlab="frequency",  
   main="frequency response (dashed lines at 0.9 and 1.1)")
 abline(h=c(0.9,1.1),lty="dashed",col="red")
 
 
-## ----cycles,fig.height=6------------------------------------------------------
-u_low <- ts(loess(u1~date,span=0.5)$fitted,start=1948,frequency=12)
-u_hi <- ts(u1 - loess(u1~date,span=0.1)$fitted,start=1948,frequency=12)
+## ----loess_code,echo=T,eval=F--------------------------------------------
+## u1_loess <- loess(u1~date,span=0.5)
+## plot(date,u1,type="l",col="red")
+## lines(u1_loess$x,u1_loess$fitted,type="l")
+
+
+## ----loess,echo=F,out.width="10cm"---------------------------------------
+
+## ----loess_code----------------------------------------------------------
+u1_loess <- loess(u1~date,span=0.5)
+plot(date,u1,type="l",col="red")
+lines(u1_loess$x,u1_loess$fitted,type="l")
+
+
+## ----loess_transfer_code,echo=T,eval=F-----------------------------------
+## s2 <- spectrum(ts.union(
+##   u1_ts,ts(u1_loess$fitted,start=1948,frequency=12)),
+##   plot=FALSE)
+## plot(s2$freq,s2$spec[,2]/s$spec[,1],type="l",log="y",
+##   ylab="frequency ratio", xlab="frequency", xlim=c(0,1.5),
+##   main="frequency response (dashed line at 1.0)")
+## abline(h=1,lty="dashed",col="red")
+
+
+## ----loess_transfer,eval=T,echo=F,out.width="10cm"-----------------------
+s2 <- spectrum(ts.union(
+  u1_ts,ts(u1_loess$fitted,start=1948,frequency=12)),
+  plot=FALSE)
+plot(s2$freq,s2$spec[,2]/s$spec[,1],type="l",log="y",
+  ylab="frequency ratio", xlab="frequency", xlim=c(0,1.5),
+  main="frequency response (dashed line at 1.0)")
+abline(h=1,lty="dashed",col="red")
+
+
+## ----cycles_code,echo=T,eval=F-------------------------------------------
+## u_low <- ts(loess(u1~date,span=0.5)$fitted,
+##   start=1948,frequency=12)
+## u_hi <- ts(u1 - loess(u1~date,span=0.1)$fitted,
+##   start=1948,frequency=12)
+## u_cycles <- u1 - u_hi - u_low
+## plot(ts.union(u1, u_low,u_hi,u_cycles),
+##   main="Decomposition of unemployment as trend + noise + cycles")
+
+
+## ----cycles,echo=F,eval=T,fig.width=7,fig.height=5,out.width="10cm"------
+par(mai=c(0.8,0.8,0.5,0.1))
+u_low <- ts(loess(u1~date,span=0.5)$fitted,
+  start=1948,frequency=12)
+u_hi <- ts(u1 - loess(u1~date,span=0.1)$fitted,
+  start=1948,frequency=12)
 u_cycles <- u1 - u_hi - u_low
 plot(ts.union(u1, u_low,u_hi,u_cycles),
   main="Decomposition of unemployment as trend + noise + cycles")
 
 
-## ----freq_response------------------------------------------------------------
+## ----freq_response,echo=F,out.width="10cm"-------------------------------
 spec_cycle <- spectrum(ts.union(u1_ts,u_cycles),
   spans=c(3,3),
   plot=FALSE)
@@ -88,7 +165,7 @@ abline(h=1,lty="dashed",col="red")
 
 
 
-## ----show_range---------------------------------------------------------------
+## ----show_range,echo=F,out.width="10cm"----------------------------------
 cut_fraction <- 0.5
 plot(spec_cycle$freq,freq_response_cycle,
   type="l",log="y",
@@ -100,11 +177,13 @@ abline(v=freq_cycles,lty="dashed",col="blue")
 abline(h=cut_fraction,lty="dashed",col="blue")
 
 
-## ----print_range--------------------------------------------------------------
-kable(matrix(freq_cycles,nrow=1,dimnames=list("frequency",c("low","hi"))),digits=3)
+## ----print_range---------------------------------------------------------
+kable(matrix(freq_cycles,nrow=1,
+  dimnames=list("frequency",c("low","hi"))),digits=3)
 
 
-## ----zoomed_spectrum----------------------------------------------------------
+## ----zoomed_spectrum,echo=F,fig.width=6,fig.height=3,out.width="10cm"----
 s1 <- spectrum(u1_ts,spans=c(3),plot=FALSE)
-plot(s1,xlim=c(0,0.7),ylim=c(1e-2,max(s1$spec)))
+par(mai=c(1,0.8,0.1,0.1))
+plot(s1,xlim=c(0,0.7),ylim=c(1e-2,max(s1$spec)),main="")
 
