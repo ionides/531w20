c7768548e93ba9a823829c6cdd42ff2689308587
Ed Ionides <ionides@umich.edu>
2020-02-21 21:55:42 +0100

    notes09 posted

---
+++ 09/notes09.R
@@ -0,0 +1,23 @@
+## ----setup,echo=F,results=F,cache=F-------------------------------------------
+myround<- function (x, digits = 1) {
+  # taken from the broman package
+  if (digits < 1) 
+    stop("This is intended for the case digits >= 1.")
+  if (length(digits) > 1) {
+    digits <- digits[1]
+    warning("Using only digits[1]")
+  }
+  tmp <- sprintf(paste("%.", digits, "f", sep = ""), x)
+  zero <- paste0("0.", paste(rep("0", digits), collapse = ""))
+  tmp[tmp == paste0("-", zero)] <- zero
+  tmp
+}
+
+set.seed(2050320976)
+
+options(
+  keep.source=TRUE,
+  encoding="UTF-8"
+)
+
+
