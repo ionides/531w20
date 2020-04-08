7d625ca9ede5a146bf99eb840aaa760f135cab8d
Ed Ionides <ionides@umich.edu>
2020-04-07 21:09:55 -0400

    draft of ch 14

--- 14/notes14.R
+++ 14/notes14.R
@@ -1,4 +1,4 @@
-## ----setup,echo=F,results=F,cache=F--------------------------------------
+## ----setup,echo=F,results=F,cache=F-------------------------------------------
 myround<- function (x, digits = 1) {
   # taken from the broman package
   if (digits < 1) 
@@ -22,7 +22,7 @@ options(
 
 
 
-## ----prelims,echo=F,cache=F----------------------------------------------
+## ----prelims,echo=F,cache=F---------------------------------------------------
 set.seed(594709947L)
 library(ggplot2)
 theme_set(theme_bw())
@@ -35,38 +35,50 @@ library(pomp)
 stopifnot(packageVersion("pomp")>="2.0")
 
 
-## ----sp500---------------------------------------------------------------
+## ----sp500_plot_code,echo=T,eval=F--------------------------------------------
+## dat <- read.table("sp500.csv",sep=",",header=TRUE)
+## N <- nrow(dat)
+## sp500 <- dat$Close[N:1] # data are in reverse order in sp500.csv
+## plot(sp500,type="l")
+## plot(log(sp500),type="l")
+
+
+## ----sp500_plot,out.width="10cm",echo=F---------------------------------------
+par(mfrow=c(1,2),mai=c(0.8,0.8,0.1,0.3))
 dat <- read.table("sp500.csv",sep=",",header=TRUE)
 N <- nrow(dat)
 sp500 <- dat$Close[N:1] # data are in reverse order in sp500.csv
-par(mfrow=c(1,2))
 plot(sp500,type="l")
 plot(log(sp500),type="l")
 
 
-## ----garch---------------------------------------------------------------
-require(tseries)
-fit.garch <- garch(sp500,grad = "numerical", trace = FALSE)
-L.garch <- logLik(fit.garch)
+## ----data_rda_code,echo=T,eval=F----------------------------------------------
+## load(file="sp500-2002-2012.rda")
+## plot(sp500.ret.demeaned, type="l",
+##   xlab="business day (2002-2012)",ylab="demeaned S&P 500 return")
 
 
-## ----data----------------------------------------------------------------
+## ----data_rda_plot,out.width="8cm",echo=F-------------------------------------
+par(mai=c(0.8,0.8,0.1,0.1))
 load(file="sp500-2002-2012.rda")
-plot(sp500.ret.demeaned,xlab="business day (2002-2012)",ylab="demeaned S&P 500 return", type="l")
+plot(sp500.ret.demeaned, type="l",
+  xlab="business day (2002-2012)",ylab="demeaned S&P 500 return")
 
 
-## ----load_pomp-----------------------------------------------------------
-require(pomp)
+## ----garch--------------------------------------------------------------------
+require(tseries)
+fit.garch <- garch(sp500.ret.demeaned,grad = "numerical", trace = FALSE)
+L.garch <- tseries:::logLik.garch(fit.garch)
 
 
-## ----names---------------------------------------------------------------
+## ----names--------------------------------------------------------------------
 sp500_statenames <- c("H","G","Y_state")
 sp500_rp_names <- c("sigma_nu","mu_h","phi","sigma_eta")
 sp500_ivp_names <- c("G_0","H_0")
 sp500_paramnames <- c(sp500_rp_names,sp500_ivp_names)
 
 
-## ----rproc---------------------------------------------------------------
+## ----rproc--------------------------------------------------------------------
 rproc1 <- "
   double beta,omega,nu;
   omega = rnorm(0,sigma_eta * sqrt( 1- phi*phi ) * sqrt(1-tanh(G)*tanh(G)));
@@ -86,7 +98,7 @@ sp500_rproc.sim <- paste(rproc1,rproc2.sim)
 sp500_rproc.filt <- paste(rproc1,rproc2.filt)
 
 
-## ----rinit---------------------------------------------------------------
+## ----rinit--------------------------------------------------------------------
 sp500_rinit <- "
   G = G_0;
   H = H_0;
@@ -94,7 +106,7 @@ sp500_rinit <- "
 "
 
 
-## ----measure-------------------------------------------------------------
+## ----measure------------------------------------------------------------------
 sp500_rmeasure <- "
    y=Y_state;
 "
@@ -104,14 +116,14 @@ sp500_dmeasure <- "
 "
 
 
-## ----transforms----------------------------------------------------------
+## ----transforms---------------------------------------------------------------
 sp500_partrans <- parameter_trans(
   log=c("sigma_eta","sigma_nu"),
   logit="phi"
 )
 
 
-## ----sp_pomp-------------------------------------------------------------
+## ----sp_pomp------------------------------------------------------------------
 sp500.filt <- pomp(data=data.frame(
     y=sp500.ret.demeaned,time=1:length(sp500.ret.demeaned)),
   statenames=sp500_statenames,
@@ -130,18 +142,16 @@ sp500.filt <- pomp(data=data.frame(
 )
 
 
-## ----sim_pomp------------------------------------------------------------
-expit<-function(real){1/(1+exp(-real))}
-logit<-function(p.arg){log(p.arg/(1-p.arg))}
+## ----sim_pomp-----------------------------------------------------------------
 params_test <- c(
-     sigma_nu = exp(-4.5),  
-     mu_h = -0.25,  	 
-     phi = expit(4),	 
-     sigma_eta = exp(-0.07),
-     G_0 = 0,
-     H_0=0
-  )
-
+  sigma_nu = exp(-4.5),  
+  mu_h = -0.25,  	 
+  phi = expit(4),	 
+  sigma_eta = exp(-0.07),
+  G_0 = 0,
+  H_0=0
+)
+  
 sim1.sim <- pomp(sp500.filt, 
   statenames=sp500_statenames,
   paramnames=sp500_paramnames,
@@ -152,7 +162,7 @@ sim1.sim <- pomp(sp500.filt,
 sim1.sim <- simulate(sim1.sim,seed=1,params=params_test)
 
 
-## ----build_sim1.filt-----------------------------------------------------
+## ----build_sim1.filt----------------------------------------------------------
 
 sim1.filt <- pomp(sim1.sim, 
   covar=covariate_table(
@@ -161,13 +171,14 @@ sim1.filt <- pomp(sim1.sim,
     times="time"),
   statenames=sp500_statenames,
   paramnames=sp500_paramnames,
-  rprocess=discrete_time(step.fun=Csnippet(sp500_rproc.filt),delta.t=1)
+  rprocess=discrete_time(
+    step.fun=Csnippet(sp500_rproc.filt),delta.t=1)
 )
 
 
 
 
-## ----run_level-----------------------------------------------------------
+## ----run_level----------------------------------------------------------------
 run_level <- 1 
 sp500_Np <-           switch(run_level,100,1e3,2e3)
 sp500_Nmif <-         switch(run_level,10, 100,200)
@@ -176,64 +187,68 @@ sp500_Nreps_local <-  switch(run_level,10, 20, 20)
 sp500_Nreps_global <- switch(run_level,10, 20, 100)
 
 
-## ----parallel-setup,cache=FALSE------------------------------------------
-require(doParallel)
+## ----parallel-setup,cache=FALSE-----------------------------------------------
+library(doParallel)
 registerDoParallel()
+library(doRNG)
+registerDoRNG(34118892)
 
-
-## ----pf1-----------------------------------------------------------------
-stew(file=sprintf("pf1.rda",run_level),{
+## ----pf1----------------------------------------------------------------------
+stew(file=sprintf("pf1-%d.rda",run_level),{
   t.pf1 <- system.time(
-    pf1 <- foreach(i=1:sp500_Nreps_eval,.packages='pomp',
-                   .options.multicore=list(set.seed=TRUE)) %dopar% try(
-                     pfilter(sim1.filt,Np=sp500_Np)
-                   )
-  )
+    pf1 <- foreach(i=1:sp500_Nreps_eval,
+      .packages='pomp') %dopar% pfilter(sim1.filt,Np=sp500_Np))
 },seed=493536993,kind="L'Ecuyer")
 (L.pf1 <- logmeanexp(sapply(pf1,logLik),se=TRUE))
 
 
-## ----mif-----------------------------------------------------------------
+## ----mif_setup----------------------------------------------------------------
 sp500_rw.sd_rp <- 0.02
 sp500_rw.sd_ivp <- 0.1
 sp500_cooling.fraction.50 <- 0.5
+sp500_rw.sd <- rw.sd(
+  sigma_nu  = sp500_rw.sd_rp,
+  mu_h      = sp500_rw.sd_rp,
+  phi       = sp500_rw.sd_rp,
+  sigma_eta = sp500_rw.sd_rp,
+  G_0       = ivp(sp500_rw.sd_ivp),
+  H_0       = ivp(sp500_rw.sd_ivp)
+)	 
 
-stew("mif1.rda",{
-   t.if1 <- system.time({
-   if1 <- foreach(i=1:sp500_Nreps_local,
-     .packages='pomp', .combine=c) %dopar% mif2(sp500.filt,
-          params=params_test,
-          Np=sp500_Np,
-          Nmif=sp500_Nmif,
-          cooling.fraction.50=sp500_cooling.fraction.50,
-          rw.sd = rw.sd(
-                            sigma_nu  = sp500_rw.sd_rp,
-                            mu_h      = sp500_rw.sd_rp,
-                            phi       = sp500_rw.sd_rp,
-                            sigma_eta = sp500_rw.sd_rp,
-                            G_0       = ivp(sp500_rw.sd_ivp),
-                            H_0       = ivp(sp500_rw.sd_ivp)
-          )
-       )
-   L.if1 <- foreach(i=1:sp500_Nreps_local,
-     .packages='pomp', .combine=rbind) %dopar% logmeanexp(
-       replicate(sp500_Nreps_eval,
-         logLik(pfilter(sp500.filt,params=coef(if1[[i]]),Np=sp500_Np))
-       ), se=TRUE)
+
+## ----mif----------------------------------------------------------------------
+stew(file=sprintf("mif1-%d.rda",run_level),{
+  t.if1 <- system.time({
+  if1 <- foreach(i=1:sp500_Nreps_local,
+    .packages='pomp', .combine=c) %dopar% mif2(sp500.filt,
+      params=params_test,
+      Np=sp500_Np,
+      Nmif=sp500_Nmif,
+      cooling.fraction.50=sp500_cooling.fraction.50,
+      rw.sd = sp500_rw.sd)
+  L.if1 <- foreach(i=1:sp500_Nreps_local,
+    .packages='pomp', .combine=rbind) %dopar% logmeanexp(
+      replicate(sp500_Nreps_eval, logLik(pfilter(sp500.filt,
+        params=coef(if1[[i]]),Np=sp500_Np))), se=TRUE)
   })
 },seed=318817883,kind="L'Ecuyer")
 
 r.if1 <- data.frame(logLik=L.if1[,1],logLik_se=L.if1[,2],t(sapply(if1,coef)))
-if (run_level>1) 
-  write.table(r.if1,file="sp500_params.csv",append=TRUE,col.names=FALSE,row.names=FALSE)
-summary(r.if1$logLik,digits=5)
+if (run_level>1) write.table(r.if1,file="sp500_params.csv",
+  append=TRUE,col.names=FALSE,row.names=FALSE)
 
 
-## ----pairs---------------------------------------------------------------
-pairs(~logLik+sigma_nu+mu_h+phi+sigma_eta,data=subset(r.if1,logLik>max(logLik)-20))
+## ----pairs_code,echo=T,eval=F-------------------------------------------------
+## pairs(~logLik+sigma_nu+mu_h+phi+sigma_eta,
+##   data=subset(r.if1,logLik>max(logLik)-20))
 
 
-## ----box-----------------------------------------------------------------
+## ----pairs_plot,echo=F,eval=T,out.width="11cm"--------------------------------
+pairs(~logLik+sigma_nu+mu_h+phi+sigma_eta,
+  data=subset(r.if1,logLik>max(logLik)-20))
+
+
+## ----box----------------------------------------------------------------------
 sp500_box <- rbind(
  sigma_nu=c(0.005,0.05),
  mu_h    =c(-1,0),
@@ -244,40 +259,40 @@ sp500_box <- rbind(
 )
 
 
-## ----box_eval------------------------------------------------------------
-stew(file="box_eval.rda",{
+## ----box_eval-----------------------------------------------------------------
+stew(file=sprintf("box_eval-%d.rda",run_level),{
   t.box <- system.time({
     if.box <- foreach(i=1:sp500_Nreps_global,
-      .packages='pomp',.combine=c) %dopar%  
-      mif2(
-        if1[[1]],
-        params=apply(sp500_box,1,function(x)runif(1,x))
-      )
-    
+      .packages='pomp',.combine=c) %dopar% mif2(if1[[1]],
+        params=apply(sp500_box,1,function(x)runif(1,x)))
     L.box <- foreach(i=1:sp500_Nreps_global,
       .packages='pomp',.combine=rbind) %dopar% {
-                        set.seed(87932+i)
-                        logmeanexp(
-                          replicate(sp500_Nreps_eval,
-                                    logLik(pfilter(sp500.filt,params=coef(if.box[[i]]),Np=sp500_Np))
-                          ), 
-                          se=TRUE)
-                      }
+         logmeanexp(replicate(sp500_Nreps_eval, logLik(pfilter(
+	     sp500.filt,params=coef(if.box[[i]]),Np=sp500_Np))), 
+           se=TRUE)
+       }
   })
 },seed=290860873,kind="L'Ecuyer")
 
-
-r.box <- data.frame(logLik=L.box[,1],logLik_se=L.box[,2],t(sapply(if.box,coef)))
-if(run_level>1) write.table(r.box,file="sp500_params.csv",append=TRUE,col.names=FALSE,row.names=FALSE)
+r.box <- data.frame(logLik=L.box[,1],logLik_se=L.box[,2],
+  t(sapply(if.box,coef)))
+if(run_level>1) write.table(r.box,file="sp500_params.csv",
+  append=TRUE,col.names=FALSE,row.names=FALSE)
 summary(r.box$logLik,digits=5)
 
 
-## ----pairs_global--------------------------------------------------------
-pairs(~logLik+log(sigma_nu)+mu_h+phi+sigma_eta+H_0,data=subset(r.box,logLik>max(logLik)-10))
+## ----pairs_global_code,eval=F,echo=T------------------------------------------
+## pairs(~logLik+log(sigma_nu)+mu_h+phi+sigma_eta+H_0,
+##   data=subset(r.box,logLik>max(logLik)-10))
 
 
-## ----garch_benchmark-----------------------------------------------------
-library(tseries)
-fit.garch.benchmark <- garch(sp500.ret.demeaned,grad = "numerical", trace = FALSE)
-L.garch.benchmark <- logLik(fit.garch.benchmark)
+## ----pairs_global_plot,eval=T,echo=F,out.width="11cm"-------------------------
+pairs(~logLik+log(sigma_nu)+mu_h+phi+sigma_eta+H_0,
+  data=subset(r.box,logLik>max(logLik)-10))
+
+
+## ----garch_benchmark,eval=F,echo=F--------------------------------------------
+## library(tseries)
+## fit.garch.benchmark <- garch(sp500.ret.demeaned,grad = "numerical", trace = FALSE)
+## L.garch.benchmark <- tseries:::logLik.garch(fit.garch.benchmark)
 
